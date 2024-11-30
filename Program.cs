0ba93ce882f20fcff34b5dd2e3c02d0f237da1ea
khalid karam <khalid.tasks@gmail.com>
2024-11-30 18:53:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FilteringLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Sample For Distict By
+
+            var products = new[]
+               {
+                new { Id = 1, Name = "Product A", Category = "Electronics" },
+                new { Id = 2, Name = "Product B", Category = "Electronics" },
+                new { Id = 3, Name = "Product C", Category = "Clothing" }
+            };
+            #endregion
+
+
+            var numbers = new[] { 1, 2, 3, 9, 1, 5, 3, 7, 3 };
+
+            var result = products.DistinctBy(c => c.Category);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
